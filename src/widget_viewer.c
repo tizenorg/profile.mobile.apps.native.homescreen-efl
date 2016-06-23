e20a4c634ae37ec479866daab8088980e4c3a71b
juhee,park <juhee82.park@samsung.com>
2016-06-23 14:20:41 +0900

    Send touch cancel event to widget when the scroller beginning to scroll
    Change-Id: I489ae434aa92b9e74a3b51d53bdb89a74b7a3a45
    Signed-off-by: juhee,park <juhee82.park@samsung.com>

--- src/widget_viewer.c
+++ src/widget_viewer.c
@@ -32,6 +32,11 @@ static int count = 0;
 #endif
 static void __widget_viewer_delete_btn_clicked_cb(void *data, Evas_Object *obj, const char *emission, const char *source);
 
+static void __widget_viewer_created_cb(void *data, Evas_Object *obj, void *event_info);
+static void __widget_viewer_faulted_cb(void *data, Evas_Object *obj, void *event_info);
+static void __widget_viewer_update_cb(void *data, Evas_Object *obj, void *event_info);
+static void __widget_viewer_extra_info_updated_cb(void *data, Evas_Object *obj, void *event_info);
+
 void widget_viewer_init(Evas_Object *win)
 {
 	widget_viewer_evas_init(win);
@@ -46,6 +51,7 @@ void widget_viewer_add_widget(Evas_Object *parent, widget_data_t *item, int *wid
 {
 	Evas_Object *widget_layout;
 
+	LOGD("pkg : %s , Content INfo : %s", item->pkg_name, item->content_info);
 	widget_layout = elm_layout_add(parent);
 	elm_layout_file_set(widget_layout, util_get_res_file_path(EDJE_DIR"/widget.edj"), GROUP_WIDGET_LY);
 	evas_object_size_hint_weight_set(widget_layout, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
@@ -61,13 +67,19 @@ void widget_viewer_add_widget(Evas_Object *parent, widget_data_t *item, int *wid
 
 #ifndef _TEST_
 	int w, h;
-	Evas_Object *widget = widget_viewer_evas_add_widget(widget_layout, item->pkg_name, NULL, item->period);
+	Evas_Object *widget = widget_viewer_evas_add_widget(widget_layout, item->pkg_name, item->content_info, item->period);
 	evas_object_size_hint_align_set(widget, EVAS_HINT_FILL, EVAS_HINT_FILL);
 	evas_object_size_hint_weight_set(widget, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 
 	//Notify widget_size_type_e to widget application.
 	widget_service_get_size(item->type, &w, &h);
 	evas_object_resize(widget, w, h);
+
+	evas_object_smart_callback_add(widget, WIDGET_SMART_SIGNAL_WIDGET_CREATED, __widget_viewer_created_cb, item);
+	evas_object_smart_callback_add(widget, WIDGET_SMART_SIGNAL_WIDGET_FAULTED, __widget_viewer_faulted_cb, item);
+	evas_object_smart_callback_add(widget, WIDGET_SMART_SIGNAL_UPDATED, __widget_viewer_update_cb, item);
+	evas_object_smart_callback_add(widget, WIDGET_SMART_SIGNAL_EXTRA_INFO_UPDATED, __widget_viewer_extra_info_updated_cb, item);
+
 #else // for TEST
 /*	Evas_Object *widget = evas_object_rectangle_add(evas_object_evas_get(widget_layout));
 	evas_object_size_hint_align_set(widget, EVAS_HINT_FILL, EVAS_HINT_FILL);
@@ -105,6 +117,7 @@ static void __widget_viewer_delete_btn_clicked_cb(void *data, Evas_Object *obj, 
 
 void widget_viewer_send_cancel_click_event(widget_data_t *item)
 {
+	LOGD("Send to : %s", item->pkg_name);
 	Evas_Object *widget_obj = NULL;
 	widget_obj = elm_object_part_content_get(item->widget_layout, WIDGET_CONTENT);
 	widget_viewer_evas_cancel_click_event(widget_obj);
@@ -146,5 +159,57 @@ void widget_viewer_thumbnail_delete(widget_data_t *item)
 		elm_object_part_content_unset(item->widget_layout, WIDGET_THUMBNAIL);
 		evas_object_del(image);
 		elm_object_part_content_set(item->widget_layout, WIDGET_CONTENT, item->widget_content);
+	}
+}
+
+static void __widget_viewer_created_cb(void *data, Evas_Object *obj, void *event_info)
+{
+	widget_data_t *widget = (widget_data_t*) data;
+	LOGD("Pkg : %s ", widget->pkg_name);
+	const char *content_info = widget_viewer_evas_get_content_info(widget->widget_content);
+	if (content_info) {
+		if (widget->content_info) {
+			free(widget->content_info);
+			widget->content_info = NULL;
+		}
+		widget->content_info = strdup(content_info);
+		LOGD("content info :%s",widget->content_info);
+		cluster_data_update(widget);
+	}
+}
+static void __widget_viewer_faulted_cb(void *data, Evas_Object *obj, void *event_info)
+{
+	widget_data_t *widget = (widget_data_t*) data;
+	LOGD("Pkg : %s ", widget->pkg_name);
+}
+static void __widget_viewer_update_cb(void *data, Evas_Object *obj, void *event_info)
+{
+	widget_data_t *widget = (widget_data_t*) data;
+	LOGD("Pkg : %s ", widget->pkg_name);
+	const char *content_info = widget_viewer_evas_get_content_info(widget->widget_content);
+	if (content_info) {
+		if (widget->content_info) {
+			free(widget->content_info);
+			widget->content_info = NULL;
+		}
+		widget->content_info = strdup(content_info);
+		LOGD("content info :%s",widget->content_info);
+		cluster_data_update(widget);
+	}
+}
+
+static void __widget_viewer_extra_info_updated_cb(void *data, Evas_Object *obj, void *event_info)
+{
+	widget_data_t *widget = (widget_data_t*) data;
+	LOGD("Pkg : %s ", widget->pkg_name);
+	const char *content_info = widget_viewer_evas_get_content_info(widget->widget_content);
+	if (content_info) {
+		if (widget->content_info) {
+			free(widget->content_info);
+			widget->content_info = NULL;
+		}
+		widget->content_info = strdup(content_info);
+		LOGD("content info :%s",widget->content_info);
+		cluster_data_update(widget);
 	}
 }
